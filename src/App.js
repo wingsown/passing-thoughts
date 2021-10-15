e9739c439c85f0341cfec561b38bd7b1e5a112f2
Wilson <wj.buena@gmail.com>
2021-10-15 10:42:52 +0800

    Added form

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Passing Thoughts</h1>
       </header>
+
+      <main>
+        form
+      </main>
     </div>
   );
 }
