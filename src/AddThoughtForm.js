948bf0d5621e0ce3757c88c0b0bfe7ae81c1ddc9
Wilson <wj.buena@gmail.com>
2021-10-19 10:26:37 +0800

    Return method in AddThoughtForm component

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -3,17 +3,32 @@ import React, { useState } from 'react';
 export function AddThoughtForm(props) {
     const [text, setText] = useState('');
 
+    //Updates text in input field
     const handleTextChange = (event) => {
         setText(event.target.value);
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        const thought = {
+            id: 'generateId_function',
+            text: text,
+            expiresAt: 'getNewExpirationDate_function'
+        }
     }
 
-    const thought = {
-        id: 'generateId_function',
-        text: text,
-        expiresAt: 'getNewExpirationDate_function'
-    }
+    return (
+        <form 
+        className="AddThoughtForm"
+        onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            placeholder="What's on your mind?"
+            aria-label="What's on your mind?"
+            value={text}
+            />
+            <input type="submit" value="Add" />
+        </form>
+    )   
 }
