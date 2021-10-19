ed8d9b4000deeb84b2c2c66d6adeed29018e7e34
Wilson <wj.buena@gmail.com>
2021-10-19 10:21:25 +0800

    Added event handlers

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
 
 export function AddThoughtForm(props) {
-    
+    const [text, setText] = useState('');
+
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const thought = {
+        id: 'generateId_function',
+        text: text,
+        expiresAt: 'getNewExpirationDate_function'
+    }
 }
