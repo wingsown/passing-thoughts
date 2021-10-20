f038d40cc6fb195ce321056478d63cacc0823657
Wilson <wj.buena@gmail.com>
2021-10-20 11:33:15 +0800

    removed comment

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -4,7 +4,6 @@ import { generateId, getNewExpirationTime } from './Utilities';
 export function AddThoughtForm(props) {
     const [text, setText] = useState('');
 
-    //Updates text in input field
     const handleTextChange = (event) => {
         setText(event.target.value);
     }
